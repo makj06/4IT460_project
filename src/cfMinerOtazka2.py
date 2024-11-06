3eb1ac68a12ad5e209875a4443e90e3ba7f39fd4
DankoK <daniel.kubanek@mizarlab.cz>
2024-11-06 21:04:20 +0100

    cfMiner otazka 2 updated

--- src/cfMinerOtazka2.py
+++ src/cfMinerOtazka2.py
@@ -11,23 +11,22 @@ df = pd.read_excel ('../data/napo.xlsx')
 #print(df.columns)
 
 #select only required rows
-dfSelected=df[['souhrn_spokojenost_zdr_zarizeni','politicke_preference', 'spokojenost_nemocnice', 'spokojenost_prakticky_lekar', 'spokojenost_ambulantni_specialiste']]
+dfSelected=df[['souhrn_spokojenost_zdr_zarizeni','politicke_preference', 'spokojenost_nemocnice', 'spokojenost_prakticky_lekar', 'spokojenost_ambulantni_specialiste', "souhr_hodnoceni_prvku_zdrav"]]
 
 #trandform to table
 imputer = SimpleImputer(strategy="most_frequent")
 dfSelected = pd.DataFrame(imputer.fit_transform(dfSelected),columns = dfSelected.columns)
 
 #print(dfSelected.head)
-clm = cleverminer(df=df,target='souhrn_spokojenost_zdr_zarizeni',proc='CFMiner',
-                  #print('S_Down')
+clm = cleverminer(df=df,target='politicke_preference',proc='CFMiner',
                   quantifiers= {'S_Up': 2},
                   cond ={
                       'attributes':[
-                          {'name': 'politicke_preference', 'type': 'subset', 'minlen': 1, 'maxlen': 1},
-                          {'name': 'spokojenost_nemocnice', 'type': 'subset', 'minlen': 1, 'maxlen': 1},
-                          {'name': 'spokojenost_prakticky_lekar', 'type': 'subset', 'minlen': 1, 'maxlen': 1},
-                          {'name': 'spokojenost_ambulantni_specialiste', 'type': 'subset', 'minlen': 1, 'maxlen': 1},
-                      ], 'minlen':1, 'maxlen':4, 'type':'con'}
+                          #{'name': 'politicke_preference', 'type': 'subset', 'minlen': 1, 'maxlen': 1},
+                          {'name': 'spokojenost_nemocnice', 'type': 'lcut', 'minlen': 1, 'maxlen': 2},
+                          {'name': 'spokojenost_prakticky_lekar', 'type': 'lcut', 'minlen': 1, 'maxlen': 2},
+                          {'name': 'spokojenost_ambulantni_specialiste', 'type': 'lcut', 'minlen': 1, 'maxlen': 2},
+                      ], 'minlen':1, 'maxlen':2, 'type':'con'}
                   )
 
 clm.print_rulelist()
