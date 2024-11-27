84c6bd8340a701c2a60b637a74ca39af603c766e
kouv07 <kouv07@vse.cz>
2024-11-27 14:11:13 +0100

    uprava 1. 4ft miner

--- src/4ftMinerOtazka1.py
+++ src/4ftMinerOtazka1.py
@@ -11,18 +11,24 @@ df = pd.read_excel ('../data/napo.xlsx')
 #print(df.columns)
 
 #select only required rows
-dfSelected=df[['vekova_kategorie','politicke_preference', 'hodnoceni_ceskeho_zdravotnictvi', 'kraj','velikost_bydliste']]
+dfSelected=df[['vekova_kategorie','politicke_preference', 'vzdelani','hodnoceni_ceskeho_zdravotnictvi', 'kraj','velikost_bydliste', 'ekonomicke_postaveni', 'cisty_mesicni_prijem']]
 
 #trandform to table
 imputer = SimpleImputer(strategy="most_frequent")
 dfSelected = pd.DataFrame(imputer.fit_transform(dfSelected),columns = dfSelected.columns)
 
 clm = cleverminer(df=df,proc='4ftMiner',
-                  quantifiers= {'Base':50, 'conf':0.4},
+                  quantifiers= {'Base':15, 'conf':0.7},
                   ante ={
                       'attributes':[
                           {'name': 'politicke_preference', 'type': 'subset', 'minlen': 1, 'maxlen': 1},
-                      ], 'minlen':1, 'maxlen':1, 'type':'con'},
+                          {'name': 'vekova_kategorie', 'type': 'subset', 'minlen': 1, 'maxlen': 1},
+                          {'name': 'kraj', 'type': 'subset', 'minlen': 1, 'maxlen': 1},
+                          {'name': 'velikost_bydliste', 'type': 'subset', 'minlen': 1, 'maxlen': 1},
+                          {'name': 'vzdelani', 'type': 'subset', 'minlen': 1, 'maxlen': 1},
+                          {'name': 'cisty_mesicni_prijem', 'type': 'subset', 'minlen': 1, 'maxlen': 1},
+                          {'name': 'ekonomicke_postaveni', 'type': 'subset', 'minlen': 1, 'maxlen': 1},
+                      ], 'minlen':2, 'maxlen':7, 'type':'con'},
                   succ ={
                       'attributes':[
                           {'name': 'ochota_dojizdet_za_kvalitnejsi_peci', 'type': 'subset', 'minlen': 1, 'maxlen': 1}
@@ -32,5 +38,5 @@ clm = cleverminer(df=df,proc='4ftMiner',
 
 clm.print_summary()
 clm.print_rulelist()
-clm.print_rule(2)
-clm.draw_rule(2)
+clm.print_rule(1)
+clm.draw_rule(1)
