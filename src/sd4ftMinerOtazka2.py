36f040fac6f5448d4a7147d7e6452d5758401d0c
Jan Makyda <123204658+makj06@users.noreply.github.com>
2024-12-08 20:56:40 +0100

    Finální úprava (#13)
    * SD4FT miner finální úprava
    
    * SD4FT miner finální úprava - 2. otázka
    
    * Přídání komentářů

--- src/sd4ftMinerOtazka2.py
+++ src/sd4ftMinerOtazka2.py
@@ -6,46 +6,48 @@ from matplotlib import pyplot as plt
 from sklearn.impute import SimpleImputer
 from cleverminer import cleverminer
 
-#read the data
+#Načtení dat
 df = pd.read_excel ('../data/napo.xlsx')
 #print(df.columns)
 
-#select only required rows
+#Výběr sloupců používaných při proceduře
 dfSelected=df[['vekova_kategorie','vzdelani', 'pohlavi', 'kraj', 'velikost_bydliste', 'politicke_preference', 'hodnoceni_ceskeho_zdravotnictvi']]
 
 
-#transform to table
+#Transformace dat do tabulky
 imputer = SimpleImputer(strategy="most_frequent")
 dfSelected = pd.DataFrame(imputer.fit_transform(dfSelected),columns = dfSelected.columns)
 
-#print(dfSelected.head)
-
-
+#Provedení SD4FT Miner procedury
 clm = cleverminer(df=df,proc='SD4ftMiner',
-                  quantifiers= {'Base1':30,'Base2':30, 'Ratioconf':2},
+                  quantifiers= {'Base1':20,'Base2':20, 'Ratioconf':2},
                   ante ={
                       'attributes':[
-                          {'name': 'vekova_kategorie', 'type': 'subset', 'minlen': 1, 'maxlen': 3},
-                          {'name': 'vzdelani', 'type': 'subset', 'minlen': 1, 'maxlen': 3},
-                          {'name': 'kraj', 'type': 'subset', 'minlen': 1, 'maxlen': 2},
-                          {'name': 'velikost_bydliste', 'type': 'subset', 'minlen': 1, 'maxlen': 2},
-                          {'name': 'pohlavi', 'type': 'subset', 'minlen': 1, 'maxlen': 2},
+                          {'name': 'vekova_kategorie', 'type': 'seq', 'minlen': 1, 'maxlen': 2}, #Typ proměnné je sequence, protože se jedná o  ordinální proměnnou
+                          {'name': 'vzdelani', 'type': 'seq', 'minlen': 1, 'maxlen': 2}, #Typ proměnné je sequence, protože se jedná o  ordinální proměnnou
+                          {'name': 'kraj', 'type': 'subset', 'minlen': 1, 'maxlen': 2}, #Typ proměnné je subset, protože se jedná o nominální proměnnou.
+                          {'name': 'velikost_bydliste', 'type': 'seq', 'minlen': 1, 'maxlen': 2}, #Typ proměnné je sequence, protože se jedná o  ordinální proměnnou
+                          {'name': 'pohlavi', 'type': 'subset', 'minlen': 1, 'maxlen': 1}, #Typ proměnné je subset, protože se jedná o nominální proměnnou.
                       ], 'minlen':1, 'maxlen':4, 'type':'con'},
                   succ ={
                       'attributes':[
-                          {'name': 'hodnoceni_ceskeho_zdravotnictvi', 'type': 'lcut', 'minlen': 1, 'maxlen': 2}
+                          {'name': 'hodnoceni_ceskeho_zdravotnictvi', 'type': 'lcut', 'minlen': 1, 'maxlen': 2} #Typ proměnné je LCut, protože chceme získat z hodnocení pouze negatvní hodnoty. (hodnocení 1,2).
                       ], 'minlen':1, 'maxlen':1 , 'type':'con'},
                   frst ={
                       'attributes':[
-                          {'name': 'politicke_preference', 'type': 'one', 'value':'Spolu (ODS, KDU-ČSL, TOP 09)'},
+                          {'name': 'politicke_preference', 'type': 'one', 'value':'Spolu (ODS, KDU-ČSL, TOP 09)'}, #Typ proměnné je one, používá se protože potřebujeme pouze konkrétní hodnotu.
                       ], 'minlen':1, 'maxlen':1, 'type':'con'},
                   scnd ={
                       'attributes':[
-                          {'name': 'politicke_preference', 'type': 'one', 'value':'ANO'},
+                          {'name': 'politicke_preference', 'type': 'one', 'value':'ANO'}, #Typ proměnné je one, používá se protože potřebujeme pouze konkrétní hodnotu.
                       ], 'minlen':1, 'maxlen':1, 'type':'con'}
                   #               ,opts = {'no_optimizations':True,'max_categories':20}
                   )
-
+#Vylogování shrnujících informací
 clm.print_summary()
+#Vylogování nalezených pravidel
 clm.print_rulelist()
-clm.print_rule(1)
+#Vylogování pravidla číslo 8.
+clm.print_rule(8)
+#Zakreslení pravidla 8 do grafu.
+clm.draw_rule(8)
